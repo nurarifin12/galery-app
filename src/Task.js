444f270bcab2ed55f6c702a2555566d87fdbb4b6
nurarifin <nurrifin65@gmail.com>
2022-12-16 00:45:12 +0700

    last-commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL"; // TODO: replace this
+const studentName = "Nur Arifin"; // TODO: replace this
+const studentId = "2730718"; // TODO: replace this
+
+export { url, studentName, studentId };
